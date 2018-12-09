fa527472c859a30f71db3cf59decf853ba84a649
Lukas Neumann <lksnmnn@gmail.com>
2018-12-09 14:33:21 +0100

    Fix null pattern issue in german IE

--- src/idx.js
+++ src/idx.js
@@ -80,7 +80,7 @@ function idx<Ti, Tv>(input: Ti, accessor: (input: Ti) => Tv): ?Tv {
  * TypeError: null is not an object (evaluating 'foo.bar')
  * TypeError: null is not an object (evaluating '(" undefined ", null).bar')
  */
-const nullPattern = /^null | null$|^[^(]* null /i;
+const nullPattern = /^null | null$|^[^(]* null |nullverweis/i;
 const undefinedPattern = /^undefined | undefined$|^[^(]* undefined /i;
 
 idx.default = idx;
